c170c38aed02eddf4d1fb8ccfbd9ec96bbdb9c4f
LuisZanabria <luis.zanabria2@unmsm.edu.pe>
2022-08-28 00:08:04 -0500

    Now use fetch

--- web/src/routes/[handle].server.jsx
+++ web/src/routes/[handle].server.jsx
@@ -5,14 +5,25 @@ import {useQuery} from '@shopify/hydrogen';
 export default function Index(props) {  
 
   const {data} = useQuery(['unique', 'key'], async () => {
-    const request = await client.fetch(`*[_type == 'post']`);
-    return await request;
+
+    const request = await fetch("https://f4obnjkf.api.sanity.io/v2022-06-29/data/query/production",{
+      method: 'POST',
+
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        query: `*[_type == 'post']`
+      })
+    })
+
+    const response = request.json();
+    console.log(data);
+    return await response;
   });
 
-  console.log(data);
-  
-  console.log(props.params.handle);
   // const actualPet = data.some( pet => pet?.name?.toLowerCase() == props?.params?.handle);
+
   return (
     <div>
       <h1>{props.params.handle}</h1>
